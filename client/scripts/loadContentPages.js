7e9b3e7041f367cf5b25d9a85369f5f939dbe307
Eduardo Machado <eduardo.silvamachado07@gmail.com>
2025-03-26 19:38:02 -0300

    feat(loadContentPages): add script para carregar conteudo das paginas

---
+++ client/scripts/loadContentPages.js
@@ -0,0 +1,23 @@
+function loadContentPages(event, urlPage) {
+  if (event) {
+    event.preventDefault();
+  }
+
+  fetch(urlPage)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to load content");
+      }
+      return response.text();
+    })
+    .then((content) => {
+      document.getElementById("mainContent").innerHTML = content;
+    })
+    .catch((error) =>
+      console.log("erro ao carregar o conteudo da pagina", error)
+    );
+}
+
+window.onload = function () {
+  loadContentPages(event, "pages/login.html");
+};
