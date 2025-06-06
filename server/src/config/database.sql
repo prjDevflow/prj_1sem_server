a0d862ad683e8533ab1eb9a6da0d0f696c4d4f31
eduardo <eduardo.silvamachado07@gmail.com>
2025-06-05 22:55:19 -0300

    ajusta funcoes csv

--- server/src/config/database.sql
+++ server/src/config/database.sql
@@ -63,3 +63,26 @@ CREATE TABLE Aula (
     FOREIGN KEY (Disciplina_idDisciplina) REFERENCES Disciplina(idDisciplina),
     FOREIGN KEY (Professor_idProfessor) REFERENCES Professor(idProfessor)
 );
+
+-- Inserção de horários
+INSERT INTO Horario (HoraInicial, HoraFinal) VALUES
+('18:45:00', '19:35:00'),
+('19:35:00', '20:25:00'),
+('20:25:00', '21:25:00'),
+('21:25:00', '22:15:00'),
+('22:15:00', '23:05:00'),
+('07:30:00', '08:20:00'),
+('08:20:00', '09:10:00'),
+('09:20:00', '10:10:00'),
+('10:10:00', '11:00:00'),
+('11:10:00', '12:00:00'),
+('12:00:00', '12:50:00');
+
+INSERT INTO Semana (Dia) VALUES
+('Segunda-feira'),
+('Terça-feira'),
+('Quarta-feira'),
+('Quinta-feira'),
+('Sexta-feira'),
+('Sábado'),
+('Domingo');    
