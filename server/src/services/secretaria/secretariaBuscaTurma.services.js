966dd90e6af6b3206984233d6e76b5014da7f541
Matheus_Sales <matheusvmsales@hotmail.com>
2025-06-09 19:41:16 -0300

    SQL ATT

--- server/src/services/secretaria/secretariaBuscaTurma.services.js
+++ server/src/services/secretaria/secretariaBuscaTurma.services.js
@@ -1,31 +1,51 @@
 const db = require("../../config/db");
 async function secretariaBuscaTurno(req, res) {
   try {
-    const {curso, turno} = req.body;
+    const {
+      curso,
+      turno
+    } = req.body;
     console.log('Recebido curso:', curso);
     const result = await db.query(
       ` SELECT 
-  Turma.idTurma,
-  Turma.Nome AS nome_turma,
-  Turma.Turno,
-  Curso.Nome AS nome_curso
-FROM Turma
-JOIN Curso ON Turma.Curso_idCurso = Curso.idCurso
-WHERE Curso.Nome = $1
-  AND Turma.Turno = $2;
+    a.idAula,
+    c.Nome AS Curso,
+    t.Nome AS Turma,
+    t.Turno,
+    d.Nome AS Disciplina,
+    p.Nome AS Professor,
+    s.Nome AS Sala,
+    s.Andar,
+    sem.Dia AS DiaSemana,
+    h.HoraInicial,
+    h.HoraFinal
+FROM Aula a
+JOIN Turma t ON a.Turma_idTurma = t.idTurma
+JOIN Curso c ON t.Curso_idCurso = c.idCurso
+JOIN Disciplina d ON a.Disciplina_idDisciplina = d.idDisciplina
+JOIN Professor p ON a.Professor_idProfessor = p.idProfessor
+JOIN Sala s ON a.Sala_Numero = s.Numero
+JOIN Semana sem ON a.Semana_idSemana = sem.idSemana
+JOIN Horario h ON a.Horario_idHorario = h.idHorario
+WHERE c.Nome = $1
+  AND t.Turno = $2
 `,
       [curso, turno]
     );
     console.log("result: ", result)
     res.json(result.rows);
 
-const nomesTurmas = rows.map(({ nome_turma }) => nome_turma);
+    const nomesTurmas = rows.map(({
+      nome_turma
+    }) => nome_turma);
 
-console.log(nomesTurmas);
+    console.log(nomesTurmas);
 
   } catch (e) {
-    
-    res.status(500).json({ message: "Erro ao processar a requisição" });
+
+    res.status(500).json({
+      message: "Erro ao processar a requisição"
+    });
   }
 }
 module.exports = secretariaBuscaTurno
